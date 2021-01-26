93827bfd1fb75219622332b279167998fc0a282a
Dan6erbond <moravrav@gmail.com>
2021-01-26 14:07:05 +0100

    Replace `color` with `variant` type for `Button` component and forward ref

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,45 +1,45 @@
 import clsx from "clsx";
 import React from "react";
+import { variant } from "./variant";
 
-interface ButtonProps {
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
   icon?: React.ReactNode;
   pill?: boolean;
-  color?: "indigo" | "blue";
+  variant?: variant;
 }
 
-const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({
-  icon,
-  children,
-  pill = false,
-  color = "blue",
-  className,
-  ...props
-}) => {
-  return (
-    <button
-      className={clsx([
-        "px-4",
-        "py-2",
-        "border",
-        "border-transparent",
-        "text-base",
-        "text-white",
-        "font-medium",
-        `bg-${color}-600`,
-        `hover:bg-${color}-700`,
-        "flex",
-        "items-center",
-        {
-          "rounded-full": pill,
-          "rounded-md": !pill,
-        },
-        className,
-      ])}
-      {...props}>
-      {icon && <span className="mr-2">{icon}</span>}
-      {children}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    { icon, children, pill = false, variant = "primary", className, ...props },
+    ref,
+  ) => {
+    return (
+      <button
+        className={clsx([
+          "px-4",
+          "py-2",
+          "border",
+          "border-transparent",
+          "text-base",
+          "text-white",
+          "font-medium",
+          `bg-${variant}`,
+          `hover:bg-${variant}-dark`,
+          "flex",
+          "items-center",
+          {
+            "rounded-full": pill,
+            "rounded-md": !pill,
+          },
+          className,
+        ])}
+        {...props}
+        ref={ref}>
+        {icon && <span className="mr-2">{icon}</span>}
+        {children}
+      </button>
+    );
+  },
+);
 
 export default Button;
