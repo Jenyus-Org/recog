4a870e01b9bc58c17c625a8e0ec8b59e763cb89f
Dan6erbond <moravrav@gmail.com>
2021-01-28 20:58:05 +0100

    Update Button component import style and implicit return in function

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -8,38 +8,34 @@ interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
   variant?: variant;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     { icon, children, pill = false, variant = "primary", className, ...props },
     ref,
-  ) => {
-    return (
-      <button
-        className={clsx([
-          "px-4",
-          "py-2",
-          "border",
-          "border-transparent",
-          "text-base",
-          "text-white",
-          "font-medium",
-          `bg-${variant}`,
-          `hover:bg-${variant}-dark`,
-          "flex",
-          "items-center",
-          {
-            "rounded-full": pill,
-            "rounded-md": !pill,
-          },
-          className,
-        ])}
-        {...props}
-        ref={ref}>
-        {icon && <span className="mr-2">{icon}</span>}
-        {children}
-      </button>
-    );
-  },
+  ) => (
+    <button
+      className={clsx([
+        "px-4",
+        "py-2",
+        "border",
+        "border-transparent",
+        "text-base",
+        "text-white",
+        "font-medium",
+        `bg-${variant}`,
+        `hover:bg-${variant}-dark`,
+        "flex",
+        "items-center",
+        {
+          "rounded-full": pill,
+          "rounded-md": !pill,
+        },
+        className,
+      ])}
+      {...props}
+      ref={ref}>
+      {icon && <span className="mr-2">{icon}</span>}
+      {children}
+    </button>
+  ),
 );
-
-export default Button;
