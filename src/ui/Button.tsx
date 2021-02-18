4605ef2744ad3fe38a4cf87623873d014dd674bd
Dan6erbond <moravrav@gmail.com>
2021-02-18 20:07:49 +0100

    Add button elevate option for ring + shadow hover styles

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -7,11 +7,21 @@ export interface ButtonProps extends CustomComponentProps<HTMLButtonElement> {
   pill?: boolean;
   outline?: boolean;
   variant?: colorVariant | "transparent";
+  elevate?: boolean;
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { icon, children, pill, outline, variant = "default", className, ...props },
+    {
+      icon,
+      children,
+      pill,
+      outline,
+      variant = "default",
+      className,
+      elevate,
+      ...props
+    },
     ref,
   ) => (
     <button
@@ -35,30 +45,61 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         !outline && {
           "text-gray-500": variant === "light-gray",
           "bg-gray-300": variant === "light-gray",
-          "hover:bg-gray-400": variant === "light-gray",
-          "hover:bg-gray-200": variant === "default",
           "bg-primary": variant === "primary",
-          "hover:bg-primary-dark": variant === "primary",
           "bg-secondary": variant === "secondary",
-          "hover:bg-secondary-dark": variant === "secondary",
           "bg-gray-400": variant === "gray",
-          "hover:bg-gray-500": variant === "gray",
         },
         !!outline && {
           "text-primary": variant === "primary",
           "border-primary": variant === "primary",
-          "hover:bg-primary-light": variant === "primary",
           "text-secondary": variant === "secondary",
           "border-secondary": variant === "secondary",
-          "hover:bg-secondary-light": variant === "secondary",
-          "hover:text-white": variant === "primary" || variant === "secondary",
           "text-gray-500": variant === "gray",
           "border-gray-500": variant === "gray",
-          "hover:bg-gray-200": variant === "gray",
           "text-gray-400": variant === "light-gray",
           "border-gray-300": variant === "light-gray",
-          "hover:bg-gray-100": variant === "light-gray",
         },
+        !elevate && [
+          !outline && {
+            "hover:bg-gray-400": variant === "light-gray",
+            "hover:bg-gray-200": variant === "default",
+            "hover:bg-primary-dark": variant === "primary",
+            "hover:bg-secondary-dark": variant === "secondary",
+            "hover:bg-gray-500": variant === "gray",
+          },
+          !!outline && {
+            "hover:bg-primary-light": variant === "primary",
+            "hover:bg-secondary-light": variant === "secondary",
+            "hover:text-white":
+              variant === "primary" || variant === "secondary",
+            "hover:bg-gray-200": variant === "gray",
+            "hover:bg-gray-100": variant === "light-gray",
+          },
+        ],
+        !!elevate && [
+          "hover:ring-1",
+          "hover:shadow",
+          "transition-shadow",
+          "duration-200",
+          "ease-in-out",
+          !outline && {
+            "hover:ring-gray-400": variant === "light-gray",
+            "hover:ring-gray-200":
+              variant === "default" || variant === "transparent",
+            "hover:ring-primary-dark": variant === "primary",
+            "hover:ring-secondary-dark": variant === "secondary",
+            "hover:ring-gray-500": variant === "gray",
+          },
+          !!outline && {
+            "hover:ring-primary-light": variant === "primary",
+            "hover:ring-secondary-light": variant === "secondary",
+            "hover:ring-gray-200":
+              variant === "gray" ||
+              variant === "default" ||
+              variant === "transparent",
+            "hover:ring-gray-100": variant === "light-gray",
+          },
+        ],
         className,
       )}
       {...props}
