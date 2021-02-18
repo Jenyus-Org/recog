bf9ebdf3023c258be5fadd38175afc5080276432
Dan6erbond <moravrav@gmail.com>
2021-02-18 22:38:52 +0100

    Update post card styling

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,34 +1,38 @@
-import React from "react";
 import { Post as PostModel } from "@models/Post";
-import { Card, CardBody, CardContent, CardFooter, CardHeader, CardSidebar } from "@ui/Card";
-import clsx from "clsx";
 import { Button } from "@ui/Button";
-import { BsArrowUpShort, BsArrowDownShort } from "react-icons/bs";
+import {
+  Card,
+  CardBody,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardSidebar,
+} from "@ui/Card";
+import clsx from "clsx";
+import React from "react";
+import { BsArrowDownShort, BsArrowUpShort } from "react-icons/bs";
 
 interface PostProps {
   post: PostModel;
 }
 
 export const Post = ({ post }: PostProps) => {
-
   return (
-    <Card>
-      <CardSidebar>
-        <div className={clsx("flex flex-col")}>
-          <Button>
-            <BsArrowUpShort/>
-          </Button>
-          <Button>
-            <BsArrowDownShort/>
-          </Button>
-        </div>
+    <Card className={clsx("bg-white")}>
+      <CardSidebar className={clsx("grid", "gap-4")}>
+        <Button pill>
+          <BsArrowUpShort />
+        </Button>
+        <Button pill>
+          <BsArrowDownShort />
+        </Button>
       </CardSidebar>
       <CardBody>
         <CardHeader>
-          <span>{ post.title}</span>
+          <span>{post.title}</span>
         </CardHeader>
         <CardContent>
-          <span>{ post.body}</span>
+          <span>{post.body}</span>
         </CardContent>
       </CardBody>
       <CardFooter>
