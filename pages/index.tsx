072065feb5ace3d85bbbf2c44db098f619b4e5ec
Dan6erbond <moravrav@gmail.com>
2021-02-18 20:43:22 +0100

    Add favicon and metadata, `manifest.json` for PWA

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,27 @@ export default function Home() {
       <Head>
         <title>Recog</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3ECBDB" />
+        <meta name="msapplication-TileColor" content="#3ECBDB" />
+        <meta name="theme-color" content="#3ECBDB" />
       </Head>
     </div>
   );
