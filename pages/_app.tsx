8a1250a47916b2dea0ee1cc4d75675012f142dfb
Doemuu <doemu@outlook.com>
2021-03-01 20:15:26 +0100

    add quill !ERROR for SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "highlight.js/styles/atom-one-dark.css";
 import { AppProps } from "next/app";
 import { useApollo } from "src/lib/apolloClient";
 import "../styles/globals.css";
+import "quill/dist/quill.snow.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
--- pages/forum/submit.tsx
+++ pages/forum/submit.tsx
@@ -1,9 +1,11 @@
 import { Layout } from "@components/Layout";
+import { QuillEditor } from "@components/QuillEditor";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Form, FormInput } from "@ui/Form";
 import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import dynamic from "next/dynamic";
 
 export default function Submit() {
   const schema = yup.object().shape({
@@ -21,8 +23,14 @@ export default function Submit() {
       <Form onSubmit={handleSubmit(onSubmit)}>
         <FormInput type="text" name="title" ref={register} />
         <p>{errors.title?.message}</p>
+        <QuillNoSSRWrapper/>
         <FormInput type="submit" />
       </Form>
     </Layout>
   );
 }
+
+const QuillNoSSRWrapper = dynamic(
+  () => import("@components/QuillEditor"),
+  { ssr: false, loading: "loading" },
+);
