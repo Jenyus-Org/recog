31710082fe00c627f1431b9865a54fd3e580b550
Dan6erbond <moravrav@gmail.com>
2021-03-09 22:25:13 +0100

    Update `Header` to use only Chakra UI components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,46 @@
-import { FormInput } from "@ui/Form";
-import clsx from "clsx";
-import Link from "next/link";
+import { Box, Flex, Link, Spacer } from "@chakra-ui/layout";
+import { Input } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 import { BsBellFill } from "react-icons/bs";
 import { HiOutlineChatAlt } from "react-icons/hi";
 
-export const Header = () => (
-  <header
-    className={clsx(
-      "bg-white",
-      "px-6",
-      "flex",
-      "items-center",
-      "h-16",
-      "fixed",
-      "top-0",
-      "right-0",
-      "left-0",
-      "z-40",
-      "shadow-sm",
-    )}>
-    <FormInput
-      pill
-      fill="light-gray"
-      placeholder="Search..."
-      className={clsx("mr-4", "ml-auto")}
-    />
-    <div>
-      <Link href="/chat">
-        <a className={clsx("p-2", "flex", "items-center")}>
-          <HiOutlineChatAlt className={clsx("text-3xl")} />
-        </a>
-      </Link>
-    </div>
-    <div>
-      <Link href="/notifications">
-        <a className={clsx("p-2", "flex", "items-center", "mr-2")}>
-          <BsBellFill className={clsx("text-3xl")} />
-        </a>
-      </Link>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const [searchFocused, setSearchFocused] = React.useState(false);
+
+  return (
+    <Flex
+      as="header"
+      background="white"
+      px={6}
+      align="center"
+      h={16}
+      position="fixed"
+      top={0}
+      right={0}
+      left={0}
+      sx={{ zIndex: 40 }}
+      shadow="sm">
+      <Box w={64} mr={12} />
+      <Spacer />
+      <Input
+        bgColor="gray.200"
+        placeholder="Search..."
+        mr={4}
+        width={searchFocused ? "full" : "xs"}
+        onFocus={() => setSearchFocused(true)}
+        onBlur={() => setSearchFocused(false)}
+      />
+      <NextLink href="/chat" passHref>
+        <Link p={2} display="flex" alignItems="center" fontSize="3xl">
+          <HiOutlineChatAlt />
+        </Link>
+      </NextLink>
+      <NextLink href="/notifications" passHref>
+        <Link p={2} display="flex" alignItems="center" mr={2} fontSize="3xl">
+          <BsBellFill />
+        </Link>
+      </NextLink>
+    </Flex>
+  );
+};
