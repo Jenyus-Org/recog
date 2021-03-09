4d3eb627a25c989d8b3cebc4021e0147a7434735
Doemuu <doemu@outlook.com>
2021-03-09 22:58:15 +0100

    override button variants

--- src/utils/chakraTheme.tsx
+++ src/utils/chakraTheme.tsx
@@ -13,11 +13,27 @@ const colors = {
   },
 };
 
+const components = {
+  Button: {
+    variants: {
+      "menu-button": {
+        fontWeight: "normal",
+        _focus: { outline: "none", boxShadow: "none" },
+        backgroundColor: "gray.300",
+      },
+      "upvote-button": {
+        _focus: { outline: "none" },
+        borderRadius: "5px",
+      },
+    },
+  },
+};
+
 const fonts = {
   body: "Oxygen, system-ui, sans-serif",
   heading: "Comfortaa, Georgia, serif",
 };
 
-const customTheme = { colors, fonts };
+const customTheme = { colors, fonts, components };
 
 export const theme = extendTheme(customTheme);
