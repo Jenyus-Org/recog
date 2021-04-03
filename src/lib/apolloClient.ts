c3f076f681fe61864b223a85e37f92f564004d41
RaviAnand Mohabir <moravrav@gmail.com>
2021-04-03 20:21:40 +0200

    Use session for authorization in Apollo client

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { getSession } from "next-auth/client";
 
 let apolloClient: ApolloClient<any>;
 
@@ -9,14 +10,15 @@ function createApolloClient() {
     uri: "http://localhost:4000/graphql",
   });
 
-  const authLink = setContext((_, { headers }) => {
-    const token =
-      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const authLink = setContext(async (_, { headers }) => {
+    const session = await getSession();
 
     return {
       headers: {
         ...headers,
-        authorization: token ? `Bearer ${token}` : null,
+        authorization: session?.user.accessToken
+          ? `Bearer ${session?.user.accessToken}`
+          : null,
       },
     };
   });
