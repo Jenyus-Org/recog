f65295f31e5af2b92daa9a3f0960633d18a1a7cf
RaviAnand Mohabir <moravrav@gmail.com>
2021-04-03 16:49:44 +0200

    Add `camelcase` ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,5 +44,7 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    camelcase: ["error"],
+  },
   },
 };
